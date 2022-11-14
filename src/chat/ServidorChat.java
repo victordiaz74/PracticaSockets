ca982bcb14cebef8ff4695579e43c7239baa55ab
victordiaz74 <vdt21victor@gmail.com>
2022-11-14 10:28:29 +0100

    Creado servidorchat hassta difundir

--- src/chat/ServidorChat.java
+++ src/chat/ServidorChat.java
@@ -1,5 +1,71 @@
 package chat;
 
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class ServidorChat {
 
+	static LinkedList<Socket> lista = new LinkedList<Socket>();
+	static int contClientes = 0;
+	PrintStream salida = null;
+	
+	public static void main(String[] args) {
+		
+		try {
+			ServerSocket serverSocket = new ServerSocket(11150);
+			System.out.println("Servidor en marcha");
+			while(true) {
+				Socket cliente = serverSocket.accept();
+				System.out.println("Nuevo cliente se une al chat");
+				Thread hilo = new ManejadorPeticionChat(cliente);
+				hilo.start();
+				lista.add(contClientes, cliente);
+				contClientes++;
+			}
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void difundir(String nombre, String mensaje) {
+		
+		Socket s;
+		PrintWriter pw;
+		
+		for (int i = 0; i < lista.size(); i++) {
+			s = lista.get(i);
+			try {
+				pw = new PrintWriter(s.getOutputStream());
+				pw.println(nombre + s.getInetAddress().getCanonicalHostName() + "se ha conectado");
+				pw.println(mensaje);
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+	public void eliminar(Socket s) {
+		PrintWriter pw;
+		
+	}
+	
+}
+
+class ManejadorPeticionChat extends Thread {
+	
+	Socket cliente;
+	
+	public ManejadorPeticionChat(Socket s) {
+		cliente = s;
+	}
+	
+	
 }
